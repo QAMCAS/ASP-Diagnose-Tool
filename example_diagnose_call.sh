ab3b8ba6976c2db892c9d72f5f86df5828d9c3b1
David <david.kaufmann@ist.tugraz.at>
2023-04-14 15:56:20 +0200

    update example shell script

--- example_diagnose_call.sh
+++ example_diagnose_call.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-./app/dist/diagnose --index 0 -f data/asp_test_data/c499_tc_2_16.pl -a 0 --faultsize 3 --output testout --json --csv
-./app/dist/diagnose --index 1 -f data/asp_test_data/c499_tc_2_16.pl -a 0 --faultsize 3 --output testout --json --csv
-./app/dist/diagnose --index 2 -f data/asp_test_data/c499_tc_2_16.pl -a 0 --faultsize 3 --output testout --json --csv
-./app/dist/diagnose --index 3 -f data/asp_test_data/c499_tc_2_16.pl -a 0 --faultsize 3 --output testout --json --csv
-./app/dist/diagnose --index 4 -f data/asp_test_data/c499_tc_2_16.pl -a 0 --faultsize 3 --output testout --json --csv
+application/diagnose --index "0" --path data/asp_test_data --faultsize 3 --answersets 0 --output application/filename --json --csv --strongfaults "only"
